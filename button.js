a19907da87d793f7cdc0f86ed16befef3bf2ad4e
athenacats <chenalonya@gmail.com>
2025-09-25 20:00:40 +0100

    Initialize

---
+++ button.js
@@ -0,0 +1,23 @@
+export default function handler(req, res) {
+  if (req.method === "POST") {
+    return res.json({
+      canvas: {
+        content: {
+          components: [
+            {
+              type: "button",
+              id: "start_quote_details",
+              label: "Start Quote Details",
+              style: "primary",
+              action: {
+                type: "url",
+                url: "https://quoteforms.app/royalty-insurance/forms/9da9424c-32a9-42cc-a6ec-e54948bc7df4/respond",
+              },
+            },
+          ],
+        },
+      },
+    });
+  }
+  res.status(405).end();
+}
